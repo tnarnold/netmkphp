7934f40681848416afd854844d42a7dffc6ebb9c
boen_robot <boen_robot@414912dc-d686-4d1b-b8c9-e6a4cb40d665>
2011-09-26 20:00:30 +0000

    Cleaned up extrasetup.php

--- trunk/extrasetup.php
+++ trunk/extrasetup.php
@@ -1,8 +1,4 @@
 <?php
-$dependencyLocations = array();
-
-$dependencyLocations[] = __DIR__ . DIRECTORY_SEPARATOR
-    . '../../PEAR2_Net_Transmitter@sourceforge.net/trunk';
 
 $extrafiles = array(
     new \PEAR2\Pyrus\Package(
@@ -10,22 +6,3 @@ $extrafiles = array(
         . '../../PEAR2_Net_Transmitter@sourceforge.net/trunk/package.xml'
     )
 );
-
-//$extrafiles = array();
-//
-//$oldCwd = getcwd();
-//foreach ($dependencyLocations as $location) {
-//    chdir($location);
-//    foreach (
-//        new RecursiveIteratorIterator(
-//            new RecursiveDirectoryIterator(
-//                'src',
-//                RecursiveDirectoryIterator::UNIX_PATHS
-//            ),
-//            RecursiveIteratorIterator::LEAVES_ONLY
-//        ) as $path) {
-//            var_dump($path->getPathname());
-//            $extrafiles[$path->getPathname()] = $path->getRealPath();
-//    }
-//}
-//chdir($oldCwd);
